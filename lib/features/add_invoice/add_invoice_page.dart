e264c6eae4fd2e8ce0f46f10d6ca0cbddb716f9a
filip doganowski <filip.doganowski@gmail.com>
2023-02-02 18:30:00 +0100

    Add TextFormFields and polish Drawer for AddInvoicePage

--- lib/features/add_invoice/add_invoice_page.dart
+++ lib/features/add_invoice/add_invoice_page.dart
@@ -25,21 +25,77 @@ class _AddInvoicePageState extends State<AddInvoicePage> {
       drawer: Drawer(
         child: ListView(
           children: [
-            DrawerHeader(child: Text('menu')),
+            SizedBox(
+              height: 80,
+              child: DrawerHeader(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                  child: Row(
+                    children: [
+                      InkWell(
+                          onTap: () => Navigator.of(context).pop(),
+                          child: const Icon(Icons.arrow_back)),
+                      const SizedBox(
+                        width: 10,
+                      ),
+                      const Text('menu'),
+                    ],
+                  )),
+            ),
             ListTile(
-              title: Text('Add invoice'),
+              title: const Text('Add invoice'),
               onTap: () {
                 Navigator.of(context).pop();
               },
             ),
             ListTile(
-              title: Text('List of invoices'),
+              title: const Text('List of invoices'),
               onTap: () {
                 Navigator.of(context).pop();
               },
             )
           ],
         ),
+      ),
+      body: ListView(
+        children: [
+          TextFormField(
+            decoration: const InputDecoration(
+              labelText: 'Invoice no.',
+              contentPadding: EdgeInsets.all(10),
+            ),
+          ),
+          TextFormField(
+            decoration: const InputDecoration(
+              labelText: 'Contrahent',
+              contentPadding: EdgeInsets.all(10),
+            ),
+          ),
+          TextFormField(
+            decoration: const InputDecoration(
+              labelText: 'Net amount',
+              contentPadding: EdgeInsets.all(10),
+            ),
+          ),
+          TextFormField(
+            decoration: const InputDecoration(
+              labelText: 'VAT rate',
+              contentPadding: EdgeInsets.all(10),
+            ),
+          ),
+          TextFormField(
+            decoration: const InputDecoration(
+              labelText: 'Gross amount',
+              contentPadding: EdgeInsets.all(10),
+            ),
+          ),
+          TextFormField(
+            decoration: const InputDecoration(
+              labelText: 'Attachment',
+              contentPadding: EdgeInsets.all(10),
+            ),
+          ),
+        ],
       ),
     ));
   }
