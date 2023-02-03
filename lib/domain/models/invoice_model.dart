2ea47857becf51ac39595c98923ad11accadb2bb
filip doganowski <filip.doganowski@gmail.com>
2023-02-03 12:50:53 +0100

    Add InvoiceModel

---
+++ lib/domain/models/invoice_model.dart
@@ -0,0 +1,17 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'invoice_model.freezed.dart';
+
+// RUN IN TERMINAL: flutter pub run build_runner build --delete-conflicting-outputs
+
+@freezed
+class InvoiceModel with _$InvoiceModel {
+  const factory InvoiceModel({
+    required String id,
+    required String title,
+    required String contrahent,
+    required double net,
+    required int vat,
+  }) = _InvoiceModel;
+  const InvoiceModel._();
+}
