222f28e986caf58b183ca8e02654401ef572db39
filip doganowski <filip.doganowski@gmail.com>
2023-02-03 11:23:31 +0100

    Extract MyApp to app.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,8 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:moje_faktury/main.dart';
+import 'package:moje_faktury/app/app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
