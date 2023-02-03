783b5f27520b2be804d8b908b0fe236b3eddd8fb
filip doganowski <filip.doganowski@gmail.com>
2023-02-03 11:15:17 +0100

    Add Config class for flavors

--- lib/app/core/config.dart
+++ lib/app/core/config.dart
@@ -3,3 +3,18 @@ enum Flavor {
   staging,
   production,
 }
+
+class Config {
+  static Flavor appFlavor = Flavor.development;
+
+  static bool get debugShowCheckedModeBanner {
+    switch (appFlavor) {
+      case Flavor.development:
+        return true;
+      case Flavor.staging:
+        return false;
+      case Flavor.production:
+        return false;
+    }
+  }
+}
