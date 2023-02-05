b6c4519e422d8cc9ce856c4db0263b3833e465ed
filip doganowski <filip.doganowski@gmail.com>
2023-02-05 20:08:52 +0100

    Add HomePage

---
+++ lib/features/home_page/home_page.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+import 'package:moje_faktury/features/add_invoice/add_invoice_page.dart';
+import 'package:moje_faktury/features/global_widgets/background_full.dart';
+import 'package:moje_faktury/features/invoice_list/invoice_list_page.dart';
+import 'package:moje_faktury/features/menu_drawer/menu_drawer.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home'),
+      ),
+      drawer: const MenuDrawer(),
+      body: Stack(
+        children: [
+          const BackgroundFullColor(),
+          SafeArea(
+            child: Center(
+              child: Column(
+                children: [
+                  const Spacer(
+                    flex: 2,
+                  ),
+                  Image.asset(
+                    'assets/images/my_invoices_logo.png',
+                    height: 200,
+                  ),
+                  const Spacer(
+                    flex: 2,
+                  ),
+                  Row(
+                    children: [
+                      const Spacer(),
+                      SizedBox(
+                        height: 50,
+                        child: ElevatedButton.icon(
+                            onPressed: () {
+                              Navigator.of(context).push(MaterialPageRoute(
+                                  builder: (context) =>
+                                      const AddInvoicePage()));
+                            },
+                            icon: const Icon(Icons.note_add),
+                            label: const Text('Add an invoice')),
+                      ),
+                      const Spacer(),
+                      SizedBox(
+                        height: 50,
+                        child: ElevatedButton.icon(
+                            onPressed: () {
+                              Navigator.of(context).push(MaterialPageRoute(
+                                  builder: (context) =>
+                                      const InvoiceListPage()));
+                            },
+                            icon: const Icon(Icons.list),
+                            label: const Text('List of Invoices')),
+                      ),
+                      const Spacer(),
+                    ],
+                  ),
+                  const Spacer(
+                    flex: 4,
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
