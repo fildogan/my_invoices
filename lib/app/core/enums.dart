d3644aad040d6a3076a9d2c3a665b939aacba0dd
filip doganowski <filip.doganowski@gmail.com>
2023-02-05 15:08:44 +0100

    Update enums

---
+++ lib/app/core/enums.dart
@@ -0,0 +1,22 @@
+enum SortedBy {
+  titleAscending,
+  titleDescending,
+  contrahentAscending,
+  contrahentDescending,
+  netAscending,
+  netDescending,
+  grossAscending,
+  grossDescending
+}
+
+enum SelectedSort {
+  title,
+  contrahent,
+  net,
+  gross,
+}
+
+enum SortDirection {
+  ascending,
+  descending,
+}
