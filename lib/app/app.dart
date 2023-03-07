cab36fafcd1080e90dc8e64e9000f3a663a46ef7
filip doganowski <filip.doganowski@gmail.com>
2023-03-07 22:18:32 +0100

    Add currentTheme listener to app.dart

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -2,8 +2,21 @@ import 'package:flutter/material.dart';
 import 'package:my_invoices/app/core/config.dart';
 import 'package:my_invoices/features/auth/auth_gate.dart';
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    super.initState();
+    currentTheme.addListener(() {
+      setState(() {});
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -11,8 +24,11 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: Config.debugShowCheckedModeBanner,
       title: 'Invoice App',
       theme: ThemeData(
+        useMaterial3: false,
         primarySwatch: Colors.orange,
       ),
+      darkTheme: ThemeData.dark(useMaterial3: false),
+      themeMode: currentTheme.currentTheme,
       home: const AuthGate(),
     );
   }
