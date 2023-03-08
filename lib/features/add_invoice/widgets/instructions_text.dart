20d583a679b34f419d99bc27fec5331613fea75e
filip doganowski <filip.doganowski@gmail.com>
2023-03-08 19:44:27 +0100

    Update instruction text color

--- lib/features/add_invoice/widgets/instructions_text.dart
+++ lib/features/add_invoice/widgets/instructions_text.dart
@@ -7,29 +7,32 @@ class InstructionsText extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final Color textColor = Theme.of(context).brightness == Brightness.light
+        ? Colors.black54
+        : Colors.grey;
     return Center(
       child: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 30),
         child: RichText(
           textAlign: TextAlign.center,
-          text: const TextSpan(
+          text: TextSpan(
             children: [
               TextSpan(
                   text: 'Fill in all the fields and press the save ',
-                  style: TextStyle(color: Colors.black54)),
+                  style: TextStyle(color: textColor)),
               WidgetSpan(
                 alignment: PlaceholderAlignment.middle,
                 child: Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 2.0),
+                  padding: const EdgeInsets.symmetric(horizontal: 2.0),
                   child: Icon(
                     Icons.save,
-                    color: Colors.black54,
+                    color: textColor,
                   ),
                 ),
               ),
               TextSpan(
                   text: ' button to add invoice',
-                  style: TextStyle(color: Colors.black54)),
+                  style: TextStyle(color: textColor)),
             ],
           ),
         ),
